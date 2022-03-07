99dced6f385a0e1cca3d9ebf41058c4ad8f2f355
Arvid Sandin <36199701+arvidsandin@users.noreply.github.com>
2022-03-07 11:33:28 +0100

    Grid usage indicated (as if current time is 00:00)

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function makeKnob(){
     // Set properties.
     knob.setProperty('angleStart', 0 * Math.PI);
     knob.setProperty('angleEnd', 2 * Math.PI);
-    knob.setProperty('colorFG', '#000');
-    knob.setProperty('colorBG', '#bbbbbb');
+    knob.setProperty('colorFG', '#fff');
+    knob.setProperty('colorBG', '#000');
     knob.setProperty('trackWidth', 0.2);
     knob.setProperty('valMin', 0);
     knob.setProperty('valMax', 16*60);
@@ -31,6 +31,15 @@ function makeKnob(){
     elem.appendChild(node);
 }
 
+function getColor(hourOfDay, minuteOfDay){
+    //price of electricity hour by hour in Sweden at 2022-03-07
+    let gridUsage = [59.31, 58.27, 53.97, 73.04, 118.54, 154.32, 242.66, 539.08, 541.87, 504.55, 365.23, 223.44, 215.50, 212.00, 205.98, 206.79, 208.13, 216.63, 218.54, 231.47, 215.68, 202.84, 157.24, 122.00]
+    if(minuteOfDay < 30 && hourOfDay < 23){
+        hourOfDay += 1;
+    }
+    return gridUsage[hourOfDay]/Math.max.apply(Math, gridUsage);
+}
+
 function makeCircle(){
     var canvas = document.getElementById("circle");
     var context = canvas.getContext("2d");
@@ -53,6 +62,14 @@ function makeCircle(){
         context.beginPath();
         context.arc(x, y, 15, 0, Math.PI * 2);
         context.closePath();
+
+        let colorIntensity = getColor(i, 0)*255;
+        console.log(colorIntensity);
+        let r = Math.floor(255 - colorIntensity);
+        let g = Math.floor(colorIntensity);
+        let b = 0;
+        console.log(r, g, b);
+        context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
         context.fill();
 
     }
